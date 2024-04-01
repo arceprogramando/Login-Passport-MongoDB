0b88d27c2b0dfa50d972ab96939ad5cb7ed281d8
Felipe Arce <arceprogramando@gmail.com>
2024-03-31 22:03:17 -0300

    feat(backend): :coffin: Remove dead code

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import userModel from '../models/user.model.js';
 import SessionController from '../controllers/session.controller.js';
 
 const router = Router();
@@ -11,24 +10,6 @@ router.get('/', sessionController.getSession);
 router.post('/register', sessionController.registerUser);
 
 router.post('/login', sessionController.LoginUser);
-
-router.get('/welcome', async (req, res) => {
-  const { name } = req.query;
-  // eslint-disable-next-line no-console
-  console.log('🚀 ~ file: session.routes.js:69 ~ router.get ~ name:', name);
-
-  const counter = req.session?.counter;
-
-  if (!counter) {
-    req.session.counter = 1;
-    return res.send(`Te damos la bienvenida ${name}`);
-  }
-
-  req.session.user = name;
-  req.session.counter += 1;
-
-  return res.send(`Has ingresado ${name} exitosamente, unas ${req.session.counter} veces`);
-});
 
 router.get('/logout', (req, res) => {
   req.session.destroy((error) => {
